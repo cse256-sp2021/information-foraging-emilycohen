7ac629c53dee8d358175973482ac4f94d6c7583d
Emily Cohen <emilycohen@wustl.edu>
2021-03-28 20:35:42 -0500

    Changes

--- scripts/ts/dynamic-dom/sticky.js
+++ scripts/ts/dynamic-dom/sticky.js
@@ -1,18 +1,32 @@
 export function setupSticky() {
     // TODO: Copy and paste your sticky code in here!!
-
-    window.onscroll = function() {setupSticky()};
-
+   
+    window.onscroll = function() {setupHeader()};
     // Get the header
     var header = document.getElementById("main-menu-container");
+    var mturkBanner = document.getElementById("mturk-top-banner-drop-down-content")
+    // console.log(mturkBanner.className)
 
+    // window.onload = setupHeader();
     // Get the offset position of the navbar
     var sticky = header.offsetTop;
-
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
+    function setupHeader() {
+    if (window.pageYOffset > sticky && mturkBanner.className.length==0) {
+        header.classList.add("stickyBanner");
         header.classList.remove("sticky");
-    }
+         console.log("OPEN")
+    } 
+    else if (window.pageYOffset > sticky && mturkBanner.className == "none")
+    {
+        header.classList.add("sticky"); 
+        header.classList.remove("stickyBanner");
 
+        console.log("CLOSED")
+    }
+    else {
+        header.classList.remove("sticky");
+        header.classList.remove("stickyBanner");
+
+    }
+    }
  }
